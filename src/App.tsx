0deafa0f90edcf491bb1e7e8894f79a430f8f4d3
Vamshi82 <bandarivamshi723@gmail.com>
2024-12-21 12:45:00 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Vam1 from "./keshav1/Vam1";
+
+export default function App() {
+  return (
+    <div>
+      <Vam1/>
+    </div>
+  );
+}
---
+++ src/keshav1/Vam1.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function vam1() {
+  return (
+    <div>vam1</div>
+  )
+}
+
